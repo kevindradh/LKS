b61098da54631744e23e174bb7d5caa915e76af7
Kevind Radhitya <kevindradhitya27@gmail.com>
2025-12-15 08:06:33 +0700

    Add mainActivity3

--- BelajarGawean/app/src/main/java/com/example/belajargawean/MainActivity.kt
+++ BelajarGawean/app/src/main/java/com/example/belajargawean/MainActivity.kt
@@ -15,14 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
 //        val btnToRegister = findViewById<TextView>(R.id.tvToRegister)
         binding.tvToRegister.setOnClickListener {
--- BelajarGawean/app/src/main/java/com/example/belajargawean/MainActivity3.kt
+++ BelajarGawean/app/src/main/java/com/example/belajargawean/MainActivity3.kt
@@ -5,18 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.belajargawean.databinding.ActivityMain2Binding
+import com.example.belajargawean.databinding.ActivityMain3Binding
 
-class MainActivity2 : AppCompatActivity() {
-    private lateinit var binding: ActivityMain2Binding
+class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain3Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMain2Binding.inflate(layoutInflater)
+        binding = ActivityMain3Binding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.tvToLogin.setOnClickListener {
-            finish()
-        }
     }
 }
