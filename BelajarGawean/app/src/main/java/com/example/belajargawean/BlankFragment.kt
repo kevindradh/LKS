37f6da14cc5bef60b2a225eed6531298fd13152e
Kevind Radhitya <kevindradhitya27@gmail.com>
2025-12-15 10:53:18 +0700

    Add recyclerView Adapter in BlankFragment

--- BelajarGawean/app/src/main/java/com/example/belajargawean/BlankFragment.kt
+++ BelajarGawean/app/src/main/java/com/example/belajargawean/BlankFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.belajargawean.databinding.CardGaweanBinding
 import com.example.belajargawean.databinding.FragmentBlankBinding
 
 class BlankFragment : Fragment() {
@@ -19,6 +22,33 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentBlankBinding.inflate(inflater)
+
+        val adapter = object : RecyclerView.Adapter<GaweanViewHolder>() {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): GaweanViewHolder {
+                val view = CardGaweanBinding.inflate(inflater, parent, false)
+                return GaweanViewHolder(view)
+            }
+
+            override fun onBindViewHolder(
+                holder: GaweanViewHolder,
+                position: Int
+            ) {
+
+            }
+
+            override fun getItemCount(): Int {
+                return 6
+            }
+        }
+
+        binding.rvExplore.adapter = adapter
+        binding.rvExplore.layoutManager = LinearLayoutManager(parentFragment?.context)
+
         return binding.root
     }
+
+    class GaweanViewHolder(val binding: CardGaweanBinding) : RecyclerView.ViewHolder(binding.root)
 }
