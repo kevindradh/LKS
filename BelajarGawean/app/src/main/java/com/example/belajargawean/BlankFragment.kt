23281a58cc00e2e4175c5457aa0e6f220fcff5d7
Kevind Radhitya <kevindradhitya27@gmail.com>
2025-12-16 09:26:47 +0700

    Add API Connection to show job list

--- BelajarGawean/app/src/main/java/com/example/belajargawean/BlankFragment.kt
+++ BelajarGawean/app/src/main/java/com/example/belajargawean/BlankFragment.kt
@@ -6,10 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.belajargawean.databinding.CardGaweanBinding
 import com.example.belajargawean.databinding.FragmentBlankBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 class BlankFragment : Fragment() {
     private lateinit var binding: FragmentBlankBinding
@@ -24,32 +30,52 @@ class BlankFragment : Fragment() {
     ): View {
         binding = FragmentBlankBinding.inflate(inflater)
 
-        val adapter = object : RecyclerView.Adapter<GaweanViewHolder>() {
-            override fun onCreateViewHolder(
-                parent: ViewGroup,
-                viewType: Int
-            ): GaweanViewHolder {
-                val view = CardGaweanBinding.inflate(inflater, parent, false)
-                return GaweanViewHolder(view)
-            }
+        lifecycleScope.launch(Dispatchers.IO) {
+            val url = "http://10.0.2.2:5000/api/jobs"
+            val request = URL(url).openConnection() as HttpURLConnection
+            request.requestMethod = "GET"
 
-            override fun onBindViewHolder(
-                holder: GaweanViewHolder,
-                position: Int
-            ) {
-                holder.itemView.setOnClickListener {
-                    val intent = Intent(this@BlankFragment.context, MainActivity4::class.java)
-                    startActivity(intent)
+            val inputStream = request.getInputStream().bufferedReader().readText()
+            val responseCode = request.responseCode
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val convertJson = JSONObject(inputStream)
+                val getData = convertJson.getJSONArray("data")
+                lifecycleScope.launch(Dispatchers.Main) {
+                    val adapter = object : RecyclerView.Adapter<GaweanViewHolder>() {
+                        override fun onCreateViewHolder(
+                            parent: ViewGroup,
+                            viewType: Int
+                        ): GaweanViewHolder {
+                            val view = CardGaweanBinding.inflate(inflater, parent, false)
+                            return GaweanViewHolder(view)
+                        }
+
+                        override fun onBindViewHolder(
+                            holder: GaweanViewHolder,
+                            position: Int
+                        ) {
+                            var job = getData.getJSONObject(position)
+                            holder.binding.tvJobTitle.text = job.getString("name")
+                            holder.binding.tvCompanyName.text = job.getJSONObject("company")
+                                .getString("name")
+
+                            holder.itemView.setOnClickListener {
+                                val intent = Intent(this@BlankFragment.context, MainActivity4::class.java)
+                                startActivity(intent)
+                            }
+                        }
+
+                        override fun getItemCount(): Int {
+                            return getData.length()
+                        }
+                    }
+
+                    binding.rvExplore.adapter = adapter
+                    binding.rvExplore.layoutManager = LinearLayoutManager(this@BlankFragment.context)
                 }
             }
-
-            override fun getItemCount(): Int {
-                return 6
-            }
         }
-
-        binding.rvExplore.adapter = adapter
-        binding.rvExplore.layoutManager = LinearLayoutManager(this@BlankFragment.context)
 
         return binding.root
     }
