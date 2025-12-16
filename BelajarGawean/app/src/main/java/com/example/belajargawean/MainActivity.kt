318aa2f4bf8c9f609296ac2180c0274b2aa71e65
Kevind Radhitya <kevindradhitya27@gmail.com>
2025-12-16 08:20:33 +0700

    Login connection to API

--- BelajarGawean/app/src/main/java/com/example/belajargawean/MainActivity.kt
+++ BelajarGawean/app/src/main/java/com/example/belajargawean/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.belajargawean
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.belajargawean.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,9 +31,35 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
-            finish()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            lifecycleScope.launch(Dispatchers.IO) {
+                val url = "http://10.0.2.2:5000/api/auth"
+                val request = URL(url).openConnection() as HttpURLConnection
+                request.requestMethod = "POST"
+                request.setRequestProperty("Content-Type", "application/json")
+
+                val dataUser = JSONObject().apply {
+                    put("email", email)
+                    put("password", password)
+                }
+
+                val os = request.getOutputStream()
+                os.write(dataUser.toString().toByteArray())
+
+                val responseCode = request.responseCode
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val intent = Intent(this@MainActivity, MainActivity3::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "Login failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
         }
     }
 }
